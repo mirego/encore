2934b9c8743d16416e06218ecd499f0d7cdf2a11
Samuel Garneau <sgarneau@mirego.com>
2014-05-27 13:56:54 -0400

    Require active_model_serializers in Encore

--- lib/encore.rb
+++ lib/encore.rb
@@ -1,9 +1,11 @@
-require 'encore/serializer/base'
-require 'encore/serializer/instance'
-require 'encore/persister/instance'
+require 'active_model_serializers'
 
 require 'encore/version'
 require 'encore/config'
+
+require 'encore/serializer/base'
+require 'encore/serializer/instance'
+require 'encore/persister/instance'
 
 module Encore
 end
