76313a18ffcd51a3500f87f20af08e9ad332cb6a
Rémi Prévost <remi@exomel.com>
2014-05-28 09:04:50 -0400

    Remove warning when including Kaminari

--- lib/encore.rb
+++ lib/encore.rb
@@ -3,7 +3,13 @@ require 'encore/version'
 require 'active_model_serializers'
 require 'active_record'
 require 'active_support'
+
+# This is a dirty hack to fix dirty code
+# https://github.com/amatsuda/kaminari/blob/7b049067b143212a172d5bb472184eac23121f34/lib/kaminari.rb#L11-L25
+oldstderr = $stderr.dup
+$stderr = StringIO.new
 require 'kaminari'
+$stderr = oldstderr
 
 require 'encore/config'
 
