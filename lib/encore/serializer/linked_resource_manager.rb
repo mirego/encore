8b6dd52334a65109686695c3b7bfe6f588a29809
Simon Prévost <sprevost@mirego.com>
2014-06-02 14:21:20 -0400

    Add auto-inclusion of linked and main resources for other resources on the payload

--- lib/encore/serializer/linked_resource_manager.rb
+++ lib/encore/serializer/linked_resource_manager.rb
@@ -5,12 +5,21 @@ module Encore
     module LinkedResourceManager
       extend ActiveSupport::Concern
 
-      def self.add(linked_ids)
+      def self.add(linked_ids, object)
+        included_models = linked_ids.keys.map { |model| model.downcase }
+        included_models << object.klass.name.downcase
+
         linked_ids.reduce({}) do |memo, (model, ids)|
           klass = model.constantize
           serializer = Utils.fetch_serializer(klass)
 
           collection = klass.where(id: ids.to_a)
+          available_includes = klass.reflections.map do |model, _|
+            next unless included_models.include?(model.to_s)
+            model
+          end.compact
+
+          collection = collection.includes(available_includes) unless available_includes.empty?
           memo.merge! MainResourceManager.add(collection, serializer)
         end
       end
