14019c2f493457fbc59263a85f5ebbe1da26b349
Rémi Prévost <remi@exomel.com>
2014-06-09 10:32:53 -0400

    Fix Rubocop error

--- spec/encore/serializer/linked/links_includes/links_includes_has_many_spec.rb
+++ spec/encore/serializer/linked/links_includes/links_includes_has_many_spec.rb
@@ -71,8 +71,8 @@ describe Encore::Serializer do
   end
 
   context 'already included resource' do
-    it { expect(serialized[:linked][:projects][0][:links][:users]).to eq({ href: '/users?project_id=1', type: 'users' }) }
-    it { expect(serialized[:linked][:projects][1][:links][:users]).to eq({ href: '/users?project_id=2', type: 'users' }) }
+    it { expect(serialized[:linked][:projects][0][:links][:users]).to eq(href: '/users?project_id=1', type: 'users') }
+    it { expect(serialized[:linked][:projects][1][:links][:users]).to eq(href: '/users?project_id=2', type: 'users') }
   end
 
   context 'not included resource' do
